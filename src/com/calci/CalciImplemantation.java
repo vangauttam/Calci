e87bafc595742648091edc40d94188d25b3b25a1
venkatakrishna18 <venkatakrishnapurushotham@gmail.com>
2025-11-18 19:16:33 +0530

    added divison

--- src/com/calci/CalciImplemantation.java
+++ src/com/calci/CalciImplemantation.java
@@ -26,7 +26,7 @@ public class CalciImplemantation implements Calci {
 		return 0;
 	}
 	
-	@
+	
 	
 
 }
---
+++ src/com/calci/Divison.java
@@ -0,0 +1,9 @@
+package com.calci;
+
+public class Divison {
+	public double div(double num1,double num2) {
+		return num1/num2;
+		
+	}
+}
+
